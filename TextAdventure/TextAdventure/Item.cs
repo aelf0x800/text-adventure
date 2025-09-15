3193dbd97229d2188487303c3c249344104c60f0
aelf0x800 <199596498+aelf0x800@users.noreply.github.com>
2025-09-15 06:37:18 +0000

    Game is basically done (Minor fixes)

---
+++ TextAdventure/TextAdventure/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace TextAdventure
+{
+    internal enum Item
+    {
+        // Weapons
+        RustyAxe, IronSword, OpalBlade, AmethystDagger, RubyBroadSword,
+        // Potions
+        HealPotion, StrengthPotion,
+        // Consumables
+        Water, Beer, Bread, Carrot, 
+        // Misc
+        Gravel, Dirt, Stone, String, Stick, Cotton, Gauze, Tape, BunchOfNothing,
+    }
+}
