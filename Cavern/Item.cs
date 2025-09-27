1b718f15caaea3fb8ea12d9992efee19975f2d30
aelf0x800 <199596498+aelf0x800@users.noreply.github.com>
2025-09-27 22:18:43 +0100

    Rename project to Cavern

---
+++ Cavern/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace Cavern
+{
+    internal enum Item
+    {
+        // Weapons
+        RustyAxe, IronSword, OpalBlade, AmethystDagger, RubyBroadSword,
+        // Potions
+        HealPotion, StrengthPotion,
+        // Consumables
+        Water, Beer, Bread, Carrot, 
+        // Misc
+        Gravel, Dirt, Stone, String, Stick, Cotton, Gauze, Tape, BunchOfNothing,
+    }
+}
